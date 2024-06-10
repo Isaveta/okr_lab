7de098168cd541c4ebbb69505332027bd7a2f1cd
Isaveta <lizavetka013@gmail.com>
2024-06-10 08:03:00 +0300

    9 save

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Функція для діалогу з користувачем
+// Функція для діалогу з користувачем 6.1
 function userDialog() {
     var userName = prompt("Введіть ваше ім'я:");
 
@@ -46,7 +46,7 @@ function compareStrings() {
 
 
 
-//////////////////////////////////////////////
+////////////////////////////////////////////// 6.2
 // Змінити фон сторінки на 30 секунд
 document.body.style.backgroundColor = "#FFC0CB"; // Зміна фону на рожевий
 
@@ -64,7 +64,7 @@ setTimeout(function() {
 
 
 
-//
+
 // Знайти всі <p> елементи в документі
 //var allParagraphs = document.querySelectorAll("p");
 
@@ -76,3 +76,139 @@ setTimeout(function() {
 //    allParagraphs[0].textContent = itzyInfo;
 //}
 //;
+
+
+
+
+////////////// 7.1
+// Функція-обробник події призначена через атрибут onclick
+function handleClick() {
+    alert("Ви натиснули кнопку!");
+}
+
+// Пошук кнопки за id
+var button2 = document.getElementById("myButton2");
+
+// Функція-обробник для першого обробника
+function handler1() {
+    alert("Обробник 1: Ви натиснули кнопку!");
+}
+
+// Функція-обробник для другого обробника
+function handler2() {
+    alert("Обробник 2: Ви натиснули кнопку!");
+}
+
+// Призначення обробників за допомогою addEventListener
+button2.addEventListener("click", handler1);
+button2.addEventListener("click", handler2);
+
+// Об'єкт з методом handleEvent для обробки подій
+var eventHandler = {
+    handleEvent: function(event) {
+        // Виведення елемента, на якому спрацював обробник
+        alert("Спрацював обробник на елементі: " + event.currentTarget.tagName);
+    }
+};
+
+// Клас для обробки подій mouseover і mouseout
+class EventHandler {
+    constructor(element) {
+        this.messageElement = element;
+    }
+
+    handleEvent(event) {
+        if (event.type === 'mouseover') {
+            this.handleMouseOver(event);
+        } else if (event.type === 'mouseout') {
+            this.handleMouseOut(event);
+        }
+    }
+
+    handleMouseOver(event) {
+        this.messageElement.textContent = 'Ласкаво просимо!';
+        this.messageElement.style.display = 'block';
+
+        setTimeout(() => {
+            this.messageElement.style.display = 'none';
+        }, 10000);
+
+        console.log('Mouse over on:', event.currentTarget);
+    }
+
+    handleMouseOut(event) {
+        this.messageElement.textContent = 'Насолоджуйтесь сайтом!';
+        this.messageElement.style.display = 'block';
+
+        setTimeout(() => {
+            this.messageElement.style.display = 'none';
+        }, 10000);
+
+        console.log('Mouse out on:', event.currentTarget);
+    }
+}
+
+// Отримуємо елемент повідомлення
+var messageElement = document.getElementById('message');
+
+// Створюємо об'єкт EventHandler
+var eventHandler = new EventHandler(messageElement);
+
+// Додаємо обробники подій до кнопок меню
+document.querySelectorAll('.menu-button').forEach(button => {
+    button.addEventListener('mouseover', eventHandler);
+    button.addEventListener('mouseout', eventHandler);
+});
+
+// Функції для кожної дії
+function newsAction() {
+    console.log('Перехід до розділу Новини');
+    location.href = 'https://t.me/itzy_ukraine'; // Приклад посилання на сторінку з новинами
+}
+
+function albumsAction() {
+    console.log('Перехід до розділу Альбоми');
+    location.href = 'https://www.instagram.com/magic.shop._.ua?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='; // Приклад посилання на сторінку з альбомами
+}
+
+function videosAction() {
+    console.log('Перехід до розділу Ютуб');
+    location.href = 'https://www.youtube.com/@ITZY'; // Приклад посилання на сторінку з Ютуб
+}
+
+function merchAction() {
+    console.log('Перехід до розділу Мерч');
+    window.open('https://www.instagram.com/itz_isa.png?igsh=dXhnZ3pmOGRrc2F4', '_blank'); // Приклад зовнішнього посилання на мерч
+}
+
+// Отримуємо посилання на меню
+var menu = document.getElementById('menu');
+
+// Додаємо обробник подій до меню
+menu.addEventListener('click', function(event) {
+    var target = event.target;
+
+    // Перевіряємо, чи клікнули на кнопку меню
+    if (target.classList.contains('menu-button')) {
+        // Отримуємо дію з атрибута data-action
+        var action = target.getAttribute('data-action');
+
+        // Викликаємо відповідну функцію в залежності від дії
+        switch (action) {
+            case 'news':
+                newsAction();
+                break;
+            case 'albums':
+                albumsAction();
+                break;
+            case 'videos':
+                videosAction();
+                break;
+            case 'merch':
+                merchAction();
+                break;
+            default:
+                console.log('Невідома дія:', action);
+        }
+    }
+});
